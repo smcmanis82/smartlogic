a07cdd43cf84a980c8a7c76620400800dbd73ce6
smcmanis82 <sharon.mcmanis@gmail.com>
2021-08-19 13:16:16 -0500

    initial setup

---
+++ src/fileReader.js
@@ -0,0 +1,4 @@
+// read three text files
+// read one file and transform it into array then next one
+// output an array of objects into one array
+
---
+++ src/main.js
@@ -0,0 +1,3 @@
+// read three text files
+// sort the info in the text files 
+// display the results
